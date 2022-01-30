e3902459a41cf69fa3873109ab4883a2ea681638
German Fernandez <germanfernandez0306@gmail.com>
2022-01-30 15:57:58 -0300

    refactor: delete test route

--- api/source/modules/test/routes/index.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-
-export const testRouter: express.Router = express.Router();
-
-testRouter.get('/', (req, res) => {
-  res.status(200).json({
-    message: 'Hello World!',
-  });
-});
