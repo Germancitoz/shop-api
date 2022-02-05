78ac076813debda92aaff681854f86764824f0df
German Fernandez <germanfernandez0306@gmail.com>
2022-02-04 22:23:51 -0300

    feat: add utils

---
+++ api/source/modules/account/utils/auth.ts
@@ -0,0 +1,29 @@
+import bcrypt from 'bcrypt';
+import { AccountType } from '../types/Account';
+
+export const hashPassword = (password: string): Promise<string> => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      resolve(await bcrypt.hash(password, 10));
+    } catch (error) {
+      reject(error);
+    }
+  });
+};
+
+export const isValidPassword = async (
+  password: string,
+  account: AccountType | null
+): Promise<boolean> => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      if (!account) {
+        resolve(false);
+      }
+      const isValid = await bcrypt.compare(password, account!.password);
+      resolve(isValid);
+    } catch (error) {
+      reject(error);
+    }
+  });
+};
