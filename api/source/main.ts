f54b33e703028113d90265a18da3271bd54b55b1
German Fernandez <germanfernandez0306@gmail.com>
2022-02-04 22:21:41 -0300

    refactor: change console.log path

--- api/source/main.ts
+++ api/source/main.ts
@@ -5,12 +5,8 @@ import { showTitle } from './lib/title';
 (async () => {
   await showTitle('Shop-API');
   try {
-    console.log('init server');
     startServer();
-    console.log('end server');
-    console.log('init database');
     await startDatabase();
-    console.log('end database');
   } catch (error) {
     console.error(error);
     closeDatabase();
